60e64fdaad344e4871924a2ecc2230568460c696
Kangyun Wang <wky@175.147.028.145.hva.nl>
2018-04-10 13:51:28 +0200

    Moved the chat to pug template

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -1,4 +1,3 @@
 exports.homePage = (req, res) => {
-	res.sendFile('index.html', { root: './server/views' });
-	// res.render('homePage', { message: 'Hello Server' });
+	res.render('chatScreen', { message: 'Hello Server' });
 }
