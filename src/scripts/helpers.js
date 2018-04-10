50caa04f4c532212c1f0bae78cd7c740feacc518
Kangyun Wang <wky@175.147.028.145.hva.nl>
2018-04-10 14:24:19 +0200

    basic object literal ptrn done

---
+++ src/scripts/helpers.js
@@ -0,0 +1,9 @@
+
+
+exports.$ = function(element) {
+	return document.querySelector(element);
+};
+
+exports.$$ = function(element) {
+	return document.querySelectorAll(element);
+};
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,3 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-
-
-exports.$ = function(element) {
-	return document.querySelector(element);
-};
-
-exports.$$ = function(element) {
-	return document.querySelectorAll(element);
-};
-},{}],2:[function(require,module,exports){
 const h = require('./helpers');
 const $ = h.$;
 // const $$ = h.$$;
@@ -45,4 +34,3 @@ const $ = h.$;
 
 	chat.init();
 })()
-},{"./helpers":1}]},{},[2]);
